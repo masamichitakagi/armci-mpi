7c4fefc62a54d2ba262174736c9587449909a912
Jeff Hammond <jeff.science@gmail.com>
2014-01-08 15:39:54 +0000

    activate hello test

--- tests/a1/Makefile.mk
+++ tests/a1/Makefile.mk
@@ -3,12 +3,15 @@
 #
 
 check_PROGRAMS += \
+                  tests/a1/test_hello         \
                   tests/a1/test_rmw32         \
                   # end
 
 TESTS          += \
+                  tests/a1/test_hello         \
                   tests/a1/test_rmw32         \
                   # end
 
-tests_a1_test_rmw32_LDADD = libarmci.la -lm
+tests_a1_test_hello_LDADD = libarmci.la
+tests_a1_test_rmw32_LDADD = libarmci.la
 
