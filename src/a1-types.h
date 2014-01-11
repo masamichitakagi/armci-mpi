60bf7e14165d5e1317e6ce123c7a0e1c70b8ba00
Jeff Hammond <jeff.science@gmail.com>
2014-01-11 15:58:40 -0600

    minor changes and cleanup

--- src/a1-types.h
+++ src/a1-types.h
@@ -8,12 +8,12 @@
 
 typedef enum
 {
-  A1_INT32,  /**< int32            */
-  A1_INT64,  /**< int64            */
-  A1_UINT32, /**< uint32           */
-  A1_UINT64, /**< uint64           */
-  A1_FLOAT,  /**< single-precision */
-  A1_DOUBLE, /**< double-precision */
+  A1_INT32,  /* int32_t  */
+  A1_INT64,  /* int64_t  */
+  A1_UINT32, /* uint32_t */
+  A1_UINT64, /* uint64_t */
+  A1_FLOAT,  /* float    */
+  A1_DOUBLE, /* double   */
 } A1_datatype_t;
 
 typedef enum
@@ -25,39 +25,31 @@ typedef enum
 /* This is PAMI-specific.  Fix (generalize) later. */
 typedef struct
 {
-    int lactive;
-    int ractive;
-}
-A1_handle_t;
+    int local;
+    int remote;
+} A1_handle_t;
 
 static inline void types_mpi_to_a1(MPI_Datatype mpi, A1_datatype_t * a1)
 {
     switch(mpi)
     {
-        case MPI_DOUBLE: 
+        case MPI_DOUBLE:
             *a1 = A1_DOUBLE;
             break;
         case MPI_FLOAT:
             *a1 = A1_FLOAT;
             break;
-        //case MPI_INT: 
         case MPI_INT32_T:
-            *a1 = A1_INT32; 
+            *a1 = A1_INT32;
             break;
-        //case MPI_UNSIGNED: 
         case MPI_UINT32_T:
-            *a1 = A1_UINT32; 
+            *a1 = A1_UINT32;
             break;
-        //case MPI_LONG: /* only true for 64-bit systems */
-        //case MPI_LONG_LONG: 
-        //case MPI_LONG_LONG_INT: 
         case MPI_INT64_T:
-            *a1 = A1_INT64; 
+            *a1 = A1_INT64;
             break;
-        //case MPI_UNSIGNED_LONG: /* only true for 64-bit systems */
-        //case MPI_UNSIGNED_LONG_LONG: 
         case MPI_UINT64_T:
-            *a1 = A1_UINT64; 
+            *a1 = A1_UINT64;
             break;
         default:
             ARMCII_Error("INVALID TYPE (%d)",mpi);
