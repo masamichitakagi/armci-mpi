d1ac3c4f042d5d105992707a92acd67f3777be9f
Jeff Hammond <jeff.science@gmail.com>
2016-01-04 07:05:52 -0800

    strip down travis stuff [ci skip]

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -7,20 +7,7 @@ MPI_IMPL="$1"
 
 # Environment variables
 export CFLAGS="-std=c99"
-#export MPICH_CC=$CC
 export MPICC=mpicc
-
-# Capture details of build
-case "$MPI_IMPL" in
-    mpich*)
-        mpichversion
-        mpicc -show
-        ;;
-    openmpi)
-        ompi_info --arch --config --path
-        mpicc --showme:command
-        ;;
-esac
 
 # Configure and build
 ./autogen.sh
@@ -29,14 +16,4 @@ esac
 # Run unit tests
 export ARMCI_VERBOSE=1
 case "$MPI_IMPL" in
-    openmpi)
-        # OpenMPI RMA datatype support was (is?) broken...
-        export ARMCI_STRIDED_METHOD=IOV
-        export ARMCI_IOV_METHOD=BATCHED
-        make check
-        ;;
-    *)
-        # MPICH and derivatives support RMA datatypes...
-        make check
-        ;;
-esac
+make check
--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -16,9 +16,6 @@ case "$os" in
             mpich|mpich3)
                 brew install mpich
                 ;;
-            openmpi)
-                brew install openmpi
-                ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
                 exit 10
@@ -30,16 +27,7 @@ case "$os" in
         echo "Linux"
         sudo apt-get update -q
         case "$MPI_IMPL" in
-            mpich1)
-                sudo apt-get install -q cmake gfortran mpich-shmem-bin libmpich-shmem1.0-dev
-                ;;
-            mpich2)
-                sudo apt-get install -q cmake gfortran mpich2 libmpich2-3 libmpich2-dev
-                ;;
-            mpich|mpich3)
-                #sudo apt-get install -q cmake gfortran libcr0 default-jdk
-                #wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.2b3/mpich_3.2b3-1ubuntu_amd64.deb
-                #sudo dpkg -i ./mpich_3.2b3-1ubuntu_amd64.deb
+            mpich)
                 wget --no-check-certificate -q http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
                 tar -xzf mpich-3.2.tar.gz
                 cd mpich-3.2
@@ -47,9 +35,6 @@ case "$os" in
                 ../configure CC=$PRK_CC CXX=$PRK_CXX --disable-fortran --prefix=/usr
                 make -j4
                 sudo make install
-                ;;
-            openmpi)
-                sudo apt-get install -q cmake gfortran openmpi-bin openmpi-common libopenmpi-dev
                 ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
